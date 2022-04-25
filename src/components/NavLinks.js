2ad3bd9a2a5c94b65c9a53f14ddf28f80e17255f
Tetteh Yaw Precious <yawprecious072@gmail.com>
2022-04-25 16:19:44 -0400

    update

--- src/components/NavLinks.js
+++ src/components/NavLinks.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import { Link } from "react-scroll/modules";
 import styled from "styled-components";
 import { rem, device } from "../helpers/mixins";
